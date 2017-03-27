e8d6bcb1e985938da7d9e0750e9b6c97783e342a
Wagner Meyer <wagnergmeyer@gmail.com>
2017-03-27 13:55:56 -0300

    Atualiazações no Javascript. Precisa ser organizado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,36 @@
-//nudeModal object
-function nudeModal(target,trigger) {
-        this.target = target;
-        wrapNudeModalBox(target);
-        addBackground(target);
-        openNudeModal(target,trigger);
-        closeNudeModal(target,trigger);
-    }
+/*
+* ========================
+* === NUDEMODAL OBJECT ===
+* ========================
+*/
+var nudeModal = function(target,trigger,config) {
+
+    this.target = target;
+    this.config = config;
+
+    addNudeClass(target);
+    wrapNudeModalBox(target);
+    addBackground(target);
+    clickOpenNudeModal(target,trigger);
+    clickCloseNudeModal(target);
+    escCloseNudeModal(target);
+    autoOpen(config.autoOpen);
+}
+
+/*
+* =========================
+* === GENERAL FUNCTIONS ===
+* =========================
+*/
+
+//add class nude-modal
+function addNudeClass(target) {
+    $(target).addClass('nude-modal');
+}
 
 //add background
 function addBackground(target) {
-    $(target).append('<div class="nude-modal-background"></div>')
+    $(target).append('<div class="nude-modal-background close-nude-modal"></div>')
 }
 
 //create nude-modal-box class
@@ -18,28 +39,58 @@ function wrapNudeModalBox(target) {
 }
 
 //open nude modal
-function openNudeModal(target,trigger) {
+function openNudeModal(target) {
+    $(target).css('display', 'block').delay(100).queue(function(next){
+        $(target + ' .nude-modal-box').addClass('show');
+        $(target + ' .nude-modal-background').addClass('show');
+        next();
+    });
+    console.log("openNudeModal");
+}
+
+//auto open nude modal
+function autoOpenNudeModal(time) {
+
+}
+
+//click open function
+function clickOpenNudeModal(target,trigger) {
     $(trigger).click(function(event){
         event.preventDefault()
-        $(target).css('display', 'block').delay(100).queue(function(next){
-            $(target + ' .nude-modal-box').addClass('show');
-            $(target + ' .nude-modal-background').addClass('show');
-            next();
-        });
-        console.log("openNudeModal");
+        openNudeModal(target);
+    });
+}
+
+//click close function
+function clickCloseNudeModal(target) {
+    $(target + ' .close-nude-modal').on('click', function(){
+        closeNudeModal(target);
+    })
+}
+
+//esc close function
+function escCloseNudeModal(target) {
+    $(document).keypress(function(e){
+        if(e.keyCode==27) {
+            closeNudeModal(target);
+        }
     });
 }
 
 //close nude modal
-function closeNudeModal(target,trigger) {
-    $(target + ' .nude-modal-background').on('click', function(){
-        $(target + ' .nude-modal-box').removeClass('show');
-        $(target + ' .nude-modal-background').removeClass('show').delay(600).queue(function(next){
-            $(target).css('display', 'none');
-            next();
-        });
-        console.log("closeNudeModal");
-    })
+function closeNudeModal(target) {
+    $(target + ' .nude-modal-box').removeClass('show');
+    $(target + ' .nude-modal-background').removeClass('show').delay(600).queue(function(next){
+        $(target).css('display', 'none');
+        next();
+    });
+    console.log("closeNudeModal");
 }
 
-//TODO: Close Button
+/*
+TODO:
+Abrir automaticamente com delay
+Opção para exibir imagens
+Definir largura
+Definir tempo de animação
+*/
