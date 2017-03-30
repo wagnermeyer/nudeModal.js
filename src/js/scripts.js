0d7e53393d2823af52c93e88ade9228bf9b71d58
Wagner <pinkman@wagao-2.local>
2017-03-30 00:18:35 -0300

    Ajustes da opção de configuração do plugin

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,20 +10,23 @@ var nudeModal = function(target,trigger,config) {
 
     var settings = $.extend({
         autoOpen: false,
-        delay: 0,
-        width: '100%',
-        height: 'auto',
+        autoOpenDelay: 0,
+        autoClose: false,
+        autoCloseDelay: 0,
         backgroundColor: '#000000',
-        backgroundOpacity: '0.4'
+        backgroundOpacity: 0.5
     }, config);
 
-    addNudeClass(target);
-    wrapNudeModalBox(target);
-    addBackground(target);
-    clickOpenNudeModal(target,trigger);
-    clickCloseNudeModal(target);
-    escCloseNudeModal(target);
-    autoOpenNudeModal(target,settings.autoOpen,settings.delay);
+    var click = 0;
+
+    addNudeClass( target );
+    wrapNudeModalBox( target );
+    addBackground( target, settings.backgroundColor, settings.backgroundOpacity );
+    clickOpenNudeModal( target, trigger, click );
+    clickCloseNudeModal( target, click );
+    escCloseNudeModal( target );
+    autoOpenNudeModal( target, settings.autoOpen, settings.autoOpenDelay );
+    autoCloseNudeModal( target, settings.autoClose, settings.autoOpenDelay, settings.autoCloseDelay )
 }
 
 /*
@@ -35,26 +38,32 @@ var nudeModal = function(target,trigger,config) {
 //add class nude-modal
 function addNudeClass(target) {
     $(target).addClass('nude-modal');
+    console.log("addNudeClass")
 }
 
 //add background
-function addBackground(target) {
-    $(target).append('<div class="nude-modal-background close-nude-modal"></div>')
+function addBackground( target, bgColor, bgOpacity ) {
+    $('<div class="nude-modal-background close-nude-modal"></div>').appendTo(target).css("background-color", bgColor);
+    $('<style>.nude-modal-background.nm-show{ opacity:' + bgOpacity + '}</style>').appendTo('head');
+    console.log("addBackground")
 }
 
 //create nude-modal-box class
 function wrapNudeModalBox(target) {
     $(target).wrapInner('<div class="nude-modal-box"></div>')
+    console.log("wrapNudeModalBox")
 }
 
 //open nude modal
 function openNudeModal(target) {
-    $(target).css('display', 'block').delay(100).queue(function(next){
-        $(target + ' .nude-modal-box').addClass('show');
-        $(target + ' .nude-modal-background').addClass('show');
+    //block click event
+    $(target).show().delay(100).queue(function(next){
+        $(target + ' .nude-modal-box').addClass('nm-show');
+        $(target + ' .nude-modal-background').addClass('nm-show');
         next();
+        //unlock click event
     });
-    console.log("openNudeModal");
+    console.log("[OPEN]openNudeModal");
 }
 
 //auto open nude modal
@@ -65,20 +74,40 @@ function autoOpenNudeModal(target, boolean, delay) {
             openNudeModal(target)
         }, delay)
     }
+
+    console.log("autoOpenNudeModal");
+}
+
+//auto close nude modal
+function autoCloseNudeModal(target, boolean, openDelay, closeDelay) {  
+    
+    var delay = openDelay + closeDelay;
+
+    if (boolean) {
+        setTimeout(function(){
+            closeNudeModal(target)
+        }, delay)
+    }
+    console.log("autoCloseNudeModal");
 }
 
 //click open function
-function clickOpenNudeModal(target,trigger) {
+function clickOpenNudeModal(target,trigger, click) {
+
+    click++;
+
     $(trigger).click(function(event){
         event.preventDefault()
         openNudeModal(target);
+        console.log("[OPEN]clickOpenNudeModal");
     });
 }
 
 //click close function
-function clickCloseNudeModal(target) {
+function clickCloseNudeModal(target, click) {
     $(target + ' .close-nude-modal').on('click', function(){
         closeNudeModal(target);
+        console.log("[CLOSE]clickCloseNudeModal");
     })
 }
 
@@ -93,10 +122,10 @@ function escCloseNudeModal(target) {
 
 //close nude modal
 function closeNudeModal(target) {
-    $(target + ' .nude-modal-box').removeClass('show');
-    $(target + ' .nude-modal-background').removeClass('show').delay(600).queue(function(next){
+    $(target + ' .nude-modal-box').removeClass('nm-show');
+    $(target + ' .nude-modal-background').removeClass('nm-show').delay(600).queue(function(next){
         $(target).css('display', 'none');
         next();
     });
-    console.log("closeNudeModal");
+    console.log("[CLOSE]closeNudeModal");
 }
